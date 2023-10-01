7360a774c34d444cbd28d362b8fcf76b3d48a140
chamm99 <alslalsl3125@gmail.com>
2023-10-02 01:56:37 +0900

    spring 적용

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -17,9 +17,9 @@ public class OrderServiceImpl implements OrderService{
     }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
-
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
+
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
